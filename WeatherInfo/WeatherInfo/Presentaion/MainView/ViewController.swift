3dd3375d87869baf6c1d9aecf4da89cbd9c1c4bf
dahun-lee-daji <ssss6596@gmail.com>
2022-06-09 21:10:42 +0900

    Proj.init with gitignore

---
+++ WeatherInfo/WeatherInfo/Presentaion/MainView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherInfo
+//
+//  Created by 이다훈 on 2022/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello World")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
