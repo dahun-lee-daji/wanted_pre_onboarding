d7b1251799a28fb52c85f05401a2a2c0fd784069
dahun-lee-daji <ssss6596@gmail.com>
2022-06-15 23:36:20 +0900

    Chore: APIKey 추가

---
+++ WeatherInfo/WeatherInfo/Data/Network/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  WeatherInfo
+//
+//  Created by 이다훈 on 2022/06/09.
+//
+
+import Foundation
+
+struct APIKeys {
+    let info: String = "4d197916c86183fd73cd29297e699163"
+}
